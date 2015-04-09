9b2e70ec123eb61dc5c16426f2b8d422e1e2676d
Brian Runnells <brian.runnells@gmail.com>
2015-04-09 11:11:03 -0500

    Disclosed as component

--- tests/dummy/app/controllers/disclosed.js
+++ tests/dummy/app/controllers/disclosed.js
@@ -1,11 +1,4 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
-  isShowing: false,
-
-  actions: {
-    toggleShowing: function() {
-      this.toggleProperty('isShowing');
-    }
-  }
 });

317aaf1c2d468d6502adaa02382a96a7fa91c473
Brian Runnells <brian.runnells@gmail.com>
2015-04-09 11:47:40 -0500

    Remove unneeded variables

--- addon/components/purple-disclosed.js
+++ addon/components/purple-disclosed.js
@@ -4,9 +4,6 @@ import layout from '../templates/components/purple-disclosed';
 export default Ember.Component.extend({
   layout: layout,
   isShowing: false,
-  toggleText: null,
-  submitText: null,
-  cancelText: null,
   toggleAction: 'toggleShowing',
 
   actions: {

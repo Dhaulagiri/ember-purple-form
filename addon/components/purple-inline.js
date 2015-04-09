79047657c98ee149d0934f55c96dfb7862a3189c
Brian Runnells <brian.runnells@gmail.com>
2015-04-09 13:46:37 -0500

    Basic inline component functionality

--- addon/components/purple-inline.js
+++ addon/components/purple-inline.js
@@ -2,5 +2,16 @@ import Ember from 'ember';
 import layout from '../templates/components/purple-inline';
 
 export default Ember.Component.extend({
-  layout: layout
+  layout: layout,
+  isEditing: false,
+  toggleAction: 'toggleEditing',
+
+  actions: {
+    toggleEditing: function() {
+      this.toggleProperty('isEditing');
+    },
+    submit: function() {
+      this.toggleProperty('isEditing');
+    }
+  }
 });

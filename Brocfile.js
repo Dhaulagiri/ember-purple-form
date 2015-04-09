b0122dada7638c165ec27e5b5f459a40da46de39
Brian Runnells <brian.runnells@gmail.com>
2015-04-09 09:35:27 -0500

    Add Purple CSS/JS

--- Brocfile.js
+++ Brocfile.js
@@ -18,4 +18,7 @@ var app = new EmberAddon();
 // please specify an object with the list of modules as keys
 // along with the exports of each module as its value.
 
+app.import('vendor/purple/purple.css');
+app.import('vendor/purple/purple.js');
+
 module.exports = app.toTree();

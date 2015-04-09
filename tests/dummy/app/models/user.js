e29f31e74f7bc2b72bdadadee4d26be65048c01b
Brian Runnells <brian.runnells@gmail.com>
2015-04-09 13:31:03 -0500

    Display model record

---
+++ tests/dummy/app/models/user.js
@@ -0,0 +1,19 @@
+import Em from 'ember';
+import DS from 'ember-data';
+
+var User = DS.Model.extend({
+  name: DS.attr('string'),
+  email: DS.attr('string')
+});
+
+User.reopenClass({
+  FIXTURES: [
+    {
+      "id": "1",
+      "name": "Brian Runnells",
+      "email": "brian.runnells@gmail.com"
+    }
+  ]
+});
+
+export default User;

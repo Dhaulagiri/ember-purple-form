ecfdeef4b77cad16b6e442fc728f7544c14ce662
Brian Runnells <brian.runnells@gmail.com>
2015-04-09 13:17:44 -0500

    Initial work at adding model data

--- addon/components/purple-disclosed.js
+++ addon/components/purple-disclosed.js
@@ -9,11 +9,18 @@ export default Ember.Component.extend({
   actions: {
     toggleShowing: function() {
       this.toggleProperty('isShowing');
+      // if (this.get('isShowing')) {
+      //   this.sendAction('showingForm');
+      // }
     },
     submit: function() {
       // here we would need to attempt to save the model associated with the form
       // if there are validation issues, keep the form visible and show error
-
+      debugger
+      var form = this.get('targetObject').get('form');
+      this.get('targetObject').send('save').then(function(e) {
+        debugger
+      });
       // otherwise, hide the form
       this.toggleProperty('isShowing');
     }
--- tests/dummy/app/controllers/disclosed.js
+++ tests/dummy/app/controllers/disclosed.js
@@ -1,4 +1,25 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+  form: function() {
+    return this.store.createRecord('ssh-key');
+  },
+
+  actions: {
+    deleteKey: function(key) {
+      this.store.find('ssh-key', key.id).then(function(key) {
+        key.destroyRecord();
+      });
+    },
+    save: function(key) {
+      var key = this.get('key');
+      var name = this.get('name');
+
+      this.store.createRecord('ssh-key', {
+        name: name,
+        key: key
+      });
+      return false;
+    }
+  }
 });

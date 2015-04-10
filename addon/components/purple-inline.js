ac10852cd59f4dc242820140ec555b6d47201d54
Brian Runnells <brian.runnells@gmail.com>
2015-04-09 19:29:14 -0500

    WIP

--- addon/components/purple-inline.js
+++ addon/components/purple-inline.js
@@ -5,7 +5,6 @@ export default Ember.Component.extend({
   layout: layout,
   isEditing: false,
   toggleAction: 'toggleEditing',
-
   actions: {
     toggleEditing: function() {
       this.toggleProperty('isEditing');
@@ -14,15 +13,15 @@ export default Ember.Component.extend({
         // are working with.  this let's us update that value without
         // immediately updating the actual field on the model
         var model = this.get('targetObject').get('model');
-        var val = model.get(this.get('valueProp'))
+        var val = model.get(this.get('valueProp'));
         model.set(this.get('newValueProp'), val);
-      };
+      }
     },
     save: function() {
       this.toggleProperty('isEditing');
 
       var model = this.get('targetObject').get('model');
-      var val = model.get(this.get('newValueProp'))
+      var val = model.get(this.get('newValueProp'));
       model.set(this.get('valueProp'), val);
 
       this.sendAction('save');

cb781d86a40f0f3f9ada71cbe482b52ad9abd454
Brian Runnells <brian.runnells@gmail.com>
2015-04-10 15:49:29 -0500

    Add unit test for gravatar util

--- tests/unit/utils/gravatar-test.js
+++ tests/unit/utils/gravatar-test.js
@@ -3,8 +3,10 @@ import { module, test } from 'qunit';
 
 module('gravatar');
 
-// Replace this with your real tests.
-test('it works', function(assert) {
-  var result = gravatar();
-  assert.ok(result);
+test('it returns a valid gravatar', function(assert) {
+  var email = 'brian.runnells@gmail.com';
+  var expected = 'http://www.gravatar.com/avatar/9b210755bd9296a95e75b08bc14fe4aa';
+
+  var result = gravatar(email);
+  assert.equal(result, expected);
 });

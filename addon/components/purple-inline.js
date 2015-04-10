57b48e86fe1abce8f0caec950b0b250b5daf3e6d
Brian Runnells <brian.runnells@gmail.com>
2015-04-09 19:06:18 -0500

    Better inline edit
    Use backing field to handle updates so they aren’t immediate while the
    user is typing

--- addon/components/purple-inline.js
+++ addon/components/purple-inline.js
@@ -9,9 +9,22 @@ export default Ember.Component.extend({
   actions: {
     toggleEditing: function() {
       this.toggleProperty('isEditing');
+      if (this.get('isEditing')) {
+        // use 'newValueProp' as a backing field for the actual field we
+        // are working with.  this let's us update that value without
+        // immediately updating the actual field on the model
+        var model = this.get('targetObject').get('model');
+        var val = model.get(this.get('valueProp'))
+        model.set(this.get('newValueProp'), val);
+      };
     },
     save: function() {
       this.toggleProperty('isEditing');
+
+      var model = this.get('targetObject').get('model');
+      var val = model.get(this.get('newValueProp'))
+      model.set(this.get('valueProp'), val);
+
       this.sendAction('save');
     },
     cancel: function() {

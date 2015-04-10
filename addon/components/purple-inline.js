77b8461849cbc9ca3673caf7b19958665885021f
Brian Runnells <brian.runnells@gmail.com>
2015-04-10 08:15:36 -0500

    Inline update
    Build out example showing difference between live binding and using the
    backing field

--- addon/components/purple-inline.js
+++ addon/components/purple-inline.js
@@ -13,18 +13,23 @@ export default Ember.Component.extend({
         // use 'newValueProp' as a backing field for the actual field we
         // are working with.  this let's us update that value without
         // immediately updating the actual field on the model
-        var model = this.get('targetObject').get('model');
-        var val = model.get(this.get('valueProp'));
-        model.set(this.get('newValueProp'), val);
+        var valueProp = this.get('valueProp');
+        if (valueProp !== undefined) {
+          var model = this.get('targetObject').get('model');
+          var val = model.get(valueProp);
+          model.set(this.get('newValueProp'), val);
+        }
       }
     },
     save: function() {
       this.toggleProperty('isEditing');
 
-      var model = this.get('targetObject').get('model');
-      var val = model.get(this.get('newValueProp'));
-      model.set(this.get('valueProp'), val);
-
+      var newValueProp = this.get('newValueProp');
+      if (newValueProp !== undefined) {
+        var model = this.get('targetObject').get('model');
+        var val = model.get(newValueProp);
+        model.set(this.get('valueProp'), val);
+      }
       this.sendAction('save');
     },
     cancel: function() {
